16e8b9291698f0f0b3097fe1c355e21beb419432
CatherineCamerieri <Catherine_Camerieri1@baylor.edu>
2023-01-30 15:44:03 -0600

    changes

--- 2_person_dict.py
+++ 2_person_dict.py
@@ -3,5 +3,21 @@ person["fname"] = "Joe"
 person["lname"] = "Fonebone"
 person["age"] = 51
 person["spouse"] = "Edna"
-person["children"] = ["Ralph", "Betty", "Joey"]
-person["pets"] = {"dog": "Fido", "cat": "Sox"}
+person["children"] = ["Ralph", "Betty", "Joey"]  # list whithin a dictionary
+person["pets"] = {"dog": "Fido", "cat": "Sox"}  # dictionary within a dictionary
+
+print(person)
+
+# print out the name of the second child
+
+print(person["children"])
+
+# print out the name of the cat
+
+print(person["pets"])
+
+# iterate through all children and print out each child
+
+
+# print out the pets in this format:
+# type of pet: dog name of pet: Fido
